e8f6704a7e4dfd94375d087510c3f0bcf2440a54
annamaggg <annamagnusson99@gmail.com>
2023-03-21 17:46:45 +0000

    LogTransaction getTransactionDate function refactored to ensure the month is two digits long. Tests passing

--- AppFiles/LogTransaction.js
+++ AppFiles/LogTransaction.js
@@ -7,12 +7,15 @@ class LogTransaction {
 
   getTransactionDate() {
     let date = new Date();
+    let day = date.getDate();
     let month = date.getMonth()+1;
-    if (month.length < 2) {
-      month = `0${month}`
+    let year = date.getFullYear();
+    
+    if (month < 10) {
+      month = `0${month}`;
     }
 
-    return `${date.getDate()}/${month}/${date.getFullYear()}`;
+    return `${day}/${month}/${year}`;
   }
 
   returnTransaction() {
